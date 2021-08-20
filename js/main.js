31c146b50cc59c1fb0502aec244a4d0fcdffe56e
Diegooualexandre <diego.alexandre.work@gmail.com>
2021-08-20 13:46:42 -0300

    Primeiro Commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var nome = "Diego L. Alexandre";
+var idade = 31
+var frase = "Japão é o melhor time";
+alert("Nome:  " + nome + " | Idade: " + idade );
+console.log(nome);
+console.log(idade);
+alert(frase.replace("Japão","Brasil"))
